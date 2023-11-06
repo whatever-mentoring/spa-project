08f2660b90087377c3df6c201084be171dcd93a0
eunchokang <wkdb0425@gmail.com>
2023-11-06 18:16:14 +0900

    feat: webpack 설치 후 개발 서버 구축
    - webpack, webpack-cli, webpack-dev-server 설치
    - yarn init에서 entry를 index.html로 설정
    - webpack entry를 index.js로 설정
    - yarn start script 추가

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const rootElment = document.getElementById('root');
+    rootElment.textContent = 'This is a SPA project';
+})
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname),
+        },
+        open: true,
+        hot: true,
+    },
+};
